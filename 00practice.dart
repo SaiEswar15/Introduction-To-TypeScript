128e71e93608e1d40f567d9729c5a76894affc03
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-07 22:06:59 +0530

    01-06 in dart

---
+++ 00practice.dart
@@ -0,0 +1,73 @@
+void main() {
+
+  String a = "eswar";
+  // String b = "kumar";
+  String c = "kumar";
+
+  parametersTypeCheck(c,name2 : a);
+  
+}
+
+void createMapAndGetValueNotPresent() {
+    Map<String, int> obj = {
+      "roll": 123,
+      "id": 234,
+      "mobile": 86888,
+    };
+
+    print(obj["telephone"]);
+  }
+
+void createVariableAndChangeDataType()
+{
+  String str = "eswar";
+  print(str);
+
+  // str = 123;
+  // print(str);
+  //this will give you error
+}
+
+void NumberWithoutReturnType()
+{
+  var number = 12;
+  // number = "eswar";  //will give you error
+  print(number);
+  var str1 = "eswar";
+  // str1 = 12;   //will give you error
+  print(str1);
+  var bol = false;
+  // bol = "true"; //will give you an error
+  print(bol);
+  //if we give int, string, bool we cannot change to different datatype however
+
+  dynamic getDatatype = "eswar";
+  getDatatype = 13;
+  //this is similar to any
+  print(getDatatype);
+}
+
+dynamic functionDatatype()
+{
+  print("this is function");
+  return "this is function";
+}
+
+void unintializedVariable()
+{
+  var a ;
+  a = 12;
+  a = functionDatatype();
+  print(a());
+}
+
+dynamic dynamicFunction()
+{
+  print("eswar");
+}
+
+void parametersTypeCheck(String a,{required String name2,String? name1})
+{
+  print(a);
+  print("${name2}");
+}
