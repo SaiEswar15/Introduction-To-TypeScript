add747fc4c9bc0e1bed135376a3ace664a8310ef
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-07 22:00:13 +0530

    hello world

---
+++ 01intro.js
@@ -0,0 +1,11 @@
+//practice 01 
+console.log("eswar");
+//practice 02
+var obj = { name: "eswar", age: 25 };
+console.log(obj.name);
+//practice 03
+var obj1 = { name: "kumar", age: 34 };
+console.log(obj1.email);
+//explination here we can see email seen as error 
+//but this error cannot be seen in js
+//we can still execute the ts but it just shows error for you
---
+++ 01intro.ts
@@ -0,0 +1,24 @@
+//practice 01 
+console.log("eswar");
+
+//practice 02
+let obj = {name : "eswar", age : 25}
+console.log(obj.name);
+
+//practice 03
+
+//uncomment and see
+// let obj1 = {name : "kumar", age : 34}
+// console.log(obj1.email);
+
+    //explination here we can see email seen as error 
+    //but this error cannot be seen in js
+    //we can still execute the ts but it just shows error for you
+    //when executing js we can see it produces output undefined
+    //but ts gives you an error
+    //for this strict usage we use the type script
+    //umcomment and execute in both ts and js
+
+
+
+
