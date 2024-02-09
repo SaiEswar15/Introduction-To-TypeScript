b11870c62005a9a683bfbda13e586a32b8da8868
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-09 13:27:30 +0530

    array typecheck

---
+++ 16PushingArrayOfNumbers.js
@@ -0,0 +1,6 @@
+var numArr = [];
+numArr.push(2);
+numArr.push(15);
+/*numArr.push("eswar");*/
+//uncomment and see
+console.log(numArr);
---
+++ 16PushingArrayOfNumbers.ts
@@ -0,0 +1,10 @@
+let numArr:number[] = [];
+
+numArr.push(2);
+numArr.push(15);
+
+/*numArr.push("eswar");*/
+//uncomment and see
+
+console.log(numArr);
+
